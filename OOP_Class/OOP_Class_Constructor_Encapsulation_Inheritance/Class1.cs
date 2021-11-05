c2642546faec478bbf038b97d6df35abc08ac1ee
gizemguzelg <90610379+gizemguzelg@users.noreply.github.com>
2021-11-05 10:00:25 +0300

    Add files via upload

---
+++ OOP_Class/OOP_Class_Constructor_Encapsulation_Inheritance/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Constructor_Encapsulation_Inheritance
+{
+    public class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        public string City;
+        public Guid Id;
+    }
+}
---
+++ OOP_Class/OOP_Class_Constructor_Encapsulation_Inheritance/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Constructor_Encapsulation_Inheritance
+{
+    public class User
+    {
+        public string FullName;
+        public Guid UserId;
+    }
+}
