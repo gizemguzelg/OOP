ab60bd4737df7528db3c2b43df306c237ab0c258
gizemguzelg <90610379+gizemguzelg@users.noreply.github.com>
2021-11-05 09:57:26 +0300

    Add files via upload

---
+++ OOP_GenericList/OOP_GenericList/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_GenericList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            List<string> kategoriler = new List<string>() { };
+
+            List<Category> categories = new List<Category>()
+            {
+                new Category { Id= 1, Name = "Tennis Racket", Description="Best tennis racket."},
+                new Category { Id= 2, Name = "Tennis Ball", Description="Best tennis ball."},
+                new Category { Id= 3, Name = "Tennis Shoes", Description="Best tenis shoes."},
+            };
+
+            dataGridView1.DataSource = categories.ToList();
+        }
+    }
+}
