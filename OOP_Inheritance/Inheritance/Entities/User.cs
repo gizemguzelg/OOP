78cd02583088324da5a109324d66cfe27d0a898e
gizemguzelg <90610379+gizemguzelg@users.noreply.github.com>
2021-11-05 09:58:33 +0300

    Add files via upload

---
+++ OOP_Inheritance/Inheritance/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using Inheritance.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inheritance.Entities
+{
+   public class User:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value > 18)
+                    _age = value;
+                else
+                    MessageBox.Show("18 yaşından küçükler üye olamaz.");
+            }
+        }
+
+    }
+}
